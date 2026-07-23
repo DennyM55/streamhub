53839eb92c4852a8fea2fed1f48c87b87a72d8db
Denny Mathew <165308292+DennyM55@users.noreply.github.com>
2026-07-23 12:00:00 +0530

    feat: implement first User persistence with Spring Data JPA

--- src/main/java/com/dennymathew/streamhub/user/UserController.java
+++ src/main/java/com/dennymathew/streamhub/user/UserController.java
@@ -1,8 +1,6 @@
 package com.dennymathew.streamhub.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -16,6 +14,10 @@ public class UserController {
     @GetMapping("/hello")
     public String sayHello(){
         return userService.sayHello();
+    }
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
     }
 
 }
--- src/main/java/com/dennymathew/streamhub/user/UserService.java
+++ src/main/java/com/dennymathew/streamhub/user/UserService.java
@@ -4,7 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 class UserService {
+    final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     public String sayHello(){
         return "Hello Users";
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
     }
 }
